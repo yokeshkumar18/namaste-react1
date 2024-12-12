86716b5991f6522dcb8d14a00bb3c2bfb35c9709
yokeshkumar18 <yokeshk868@gmail.com>
2024-12-12 19:42:00 +0530

    episode- 01

---
+++ app.js
@@ -0,0 +1,20 @@
+const parent=React.createElement(
+    "div", 
+    {id:"parent"},
+React.createElement("div",{id:"child"},[
+React.createElement("h1",{},"I am H1 tag"),
+React.createElement("h2",{},"I am H2 tag"),
+]),
+React.createElement("div",{id:"child2"},[
+React.createElement("h1",{},"I am H1 tag"),
+React.createElement("h2",{},"I am H2 tag")
+])
+
+
+);
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
